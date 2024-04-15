47dba81af34130170895db3ffc82453a98da25e0
songhyunsik <cutaandcrazy@gmail.com>
2024-04-15 19:14:54 +0900

    2480 문제 해결 <time.h>를 이요하여 해결하는 것이 아니었다. (문제 의도를 잘 파악해야할거 같다.)

--- STAGE_2/2480.c
+++ STAGE_2/2480.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 //같은 눈이 3개가 나오면 10,000원+(같은 눈)×1,000원의 상금을 받게 된다.
 //같은 눈이 2개만 나오는 경우에는 1,000원+(같은 눈)×100원의 상금을 받게 된다.
@@ -10,28 +9,42 @@
 
 int main(void)
 {
-  int num1, num2, num3, money;
+  int num1, num2, num3;
 
-  srand(time(NULL));
+//   srand(time(NULL));
 
-  num1 = rand() % 6 + 1;
-  num2 = rand() % 6 + 1;
-  num3 = rand() % 6 + 1;
+//   num1 = rand() % 6 + 1;
+//   num2 = rand() % 6 + 1;
+//   num3 = rand() % 6 + 1;
   
-  printf("%d %d %d\n", num1, num2, num3);
-  
-  /*if(num1 == num2 && num2 == num3 && num1 == num3) {
-     money = 10000 + num1*1000;
-     printf("%d\n", money);
+//   printf("%d %d %d\n", num1, num2, num3);
+  scanf("%d %d %d", &num1, &num2, &num3);
+
+  if(num1 == num2 && num2 == num3 && num1 == num3) {
+     printf("%d\n", 10000 + num1 * 1000);
   }
-  else if(num1 == num2 && num2 == num3 && num1 == num3) {
-     money = 1000 + num1*100;
-     printf("%d\n", money);
+  else if(num1 == num2 || num2 == num3 || num1 == num3) {
+      if(num1 == num2) {
+         printf("%d\n", 1000 + num1 * 100);
+      }
+      else if(num2 == num3) {
+         printf("%d\n", 1000 + num2 * 100);
+      }
+      else if(num1 == num3) {
+         printf("%d\n", 1000 + num1 * 100);
+      }
   }
-  else if(num1 == num2 && num2 == num3 && num1 == num3) {
-     money = num1*100;
-     printf("%d\n", money);
-  }*/
+  else if(num1 != num2 && num2 != num3 && num1 != num3) {
+     if(num1 > num2 && num1 > num3) {
+      printf("%d\n", num1 * 100);
+     }
+     else if(num2 > num1 && num2 > num3) {
+      printf("%d\n", num2 * 100);
+     }
+     else if(num3 > num1 && num3 > num2) {
+      printf("%d\n", num3 * 100);
+     }
+  }
    
 
    return 0;

031d7c8bcd617cdc0c2b0ac3c3706212eb02a1ed
songhyunsik <cutaandcrazy@gmail.com>
2024-05-27 22:23:20 +0900

    2024/05/27 백준 스터디 --> 답은 나오지만 채점은 틀렸다고 한다...문제 조건을 더 확인해봐야 할것같다.

--- STAGE_4/5597.c
+++ STAGE_4/5597.c
@@ -2,21 +2,25 @@
 
 int main()
 {
-    int student[29], roll_book[31], cnt;
+    int student[29], roll_book[31], cnt[30]={0};
 
     for(int i = 1; i <= 28; i++) {
         scanf("%d", &student[i]);
     }
-
     for(int i = 1; i <= 30; i++) {
         roll_book[i] = i;
     }
-
-    for(int i = 1; i <= 30; i++) {
-        
+    for(int i = 1; i <= 28; i++) {
+        for(int j = 1; j <= 30; j++) {
+            if(student[i] == roll_book[j]) {
+                cnt[j] = 1;
+            }
+        }
     }
-
-    
-
+    for(int j = 1; j <= 30; j++) {
+        if(cnt[j] == 0) {
+            printf("%d\n", roll_book[j]);
+        }
+    }
     return 0;
 }
